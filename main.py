1bdcdfdefa88b902cfb52ea9ef925b14eceba9f0
23Mandla <mandlambolekwa@gmail.com>
2025-02-20 22:41:29 +0200

    Working on scheduling meeting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import click
+from click.testing import CliRunner
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, auth
 import requests
 from constance import const
 
@@ -22,13 +23,14 @@ def sign_up(email, password):
         # http request
         response = requests.post(const.SIGN_UP, json = user, headers = {"Content-Type" : "application/json"})
         if response.status_code == 200:
-            print("Succesfully registered : ", response.json())
+            click.echo("Succesfully registered")
         else:
-            print("An error occured", response.json())
+            click.echo("An error occured", response.json())
             
     except Exception as error:
-        print(error)
+        click.echo(error)
 
+# sign in
 def sign_in(email, password):
     try:
         user_ = {
@@ -40,56 +42,85 @@ def sign_in(email, password):
         # http request
         response = requests.post(const.SIGN_IN, json = user_, headers = {"Content-Type" : "application/json"})
         if response.status_code == 200:
-            print("Succesfully loged in : ")
-            result = response.json()
-            return result.get('idToken')
+            click.echo("Succesfully loged in : ")
+            return email
         else:
-            print("An error occured", response.json())
+            click.echo("An error occured", response.json())
             
     except Exception as error:
-        print(error)
+        click.echo(error)
 
 #arguments using click
-@click.option("--name", "-n", prompt = "Enter your name ", help = "Your name")
-@click.option("--email", "-n", prompt = "Enter your email ", help = "Your email address")
-@click.option("--role", "-n", prompt = "Are you a mentor or mentee ", help = "Mentor or mentee")
-@click.option("--expertise", "-n", prompt = "Experience [beginner, intermediate, senior] ", help = "Your experience")
 
-# code in progress
-@click.command()
-def store_user_details(name, email, role, expertise):
+def store_user_details(id, name, email, role, expertise):
 
-    id = store_user_details.id
-    db.collection("users").document(id).set({
+    user = db.collection("users").document(id)
+    userExist = user.get()
+
+    if not userExist.exists:
+        db.collection("users").document(id).set({
         "name" : name,
         "email" : email,
         "role" : role,
         "expertise" : expertise
-    })
+         })
+
+        click.echo("Succsfully registered")
+
+    else:
+        raise ValueError("User already exist")
+    
+def get_firebase_user_id(email):
+    """Fetches the Firebase Authentication UID based on email."""
+    try:
+        user = auth.get_user_by_email(email)
+        return user.uid
+    
+    except Exception as e:
+        click.echo(e)
+    
+# schedule meeting
+def meeting(mentor_id, mentee_id, time, status):
+    # TODO  # get mentor / mentee from the database
+
+    meeting_id = "meeting_1" 
+    meeting_data = {
+        "mentor_id": mentor_id,
+        "mentee_id": mentee_id,
+        "time": time,
+        "status": status
+    }
+
+    db.collection("meetings").document(meeting_id).set(meeting_data)
+    click.echo(f"Meeting scheduled successfully")
 
 #arguments using click
 @click.option("--password", "-n", prompt = "Enter your password ", help = "Your name")
 @click.option("--email", "-n", prompt = "Enter your email ", help = "Your name")
 @click.option("--sign", "-n", prompt = "Would you like to sign in or up ", help = "Your name")
-
-# code in progress
 @click.command()
+
 def main(sign, email, password):
-   
+    
     if sign == "in":
-        id = sign_in(email, password)
-        action = input("Here is what you can do : ['register'] ? ")
-        while True:
-            
+        action = input("Here is what you can do : ['register'] if registered view mentor ? ")
+        
+        userEmail = sign_in(email, password)
+        id = get_firebase_user_id(userEmail)
+        if userEmail:
             if action == "register":
-                store_user_details.id = id
+             
+                name = click.prompt("Enter your name : ")
+                role = click.prompt("Are you a mentor or mentee? : ")
+                expertise = click.prompt("Experience [beginner, intermediate, senior]")
+
                 try:
-                    store_user_details()
-                    print("User details stored successfully!")
+                    store_user_details(id, name, userEmail,  role, expertise)
+                    click.echo("User details stored successfully!")
                 except Exception as e:
-                    print(f"Error storing user details: {e}")
-                    continue
-                input("Would like to perform other action ?")
+                    click.echo(f"Error storing user details: {e}")
+            else:
+                meeting()
     else:
         sign_up(email, password)
         
